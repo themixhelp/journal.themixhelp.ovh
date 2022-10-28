5df9cb0fb85115f1a59e85b7079d4e093bee2ace
themixhelp <themixhelp@outlook.com>
2022-10-28 19:17:09 +0200

    beautify files with prettier

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,61 +5,61 @@
 		<h1>themixhelp!</h1>
 
 		<aside>
-            <a href="#">tryb nocny</a>
+			<a href="#">tryb nocny</a>
 			<a href="https://github.com/themixhelp/journal.themixhelp.ovh">github</a>
 		</aside>
 	</section>
 </template>
 
 <style scoped>
-    .navbar-wrapper {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+	.navbar-wrapper {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 
-        padding-bottom: 1.5rem;
+		padding-bottom: 1.5rem;
 
-        font-size: 1.25rem;
-    }
+		font-size: 1.25rem;
+	}
 
-    .navbar-wrapper h1 {
-        font-size: 2rem;
-    }
+	.navbar-wrapper h1 {
+		font-size: 2rem;
+	}
 
-    .navbar-wrapper aside {
-        display: flex;
+	.navbar-wrapper aside {
+		display: flex;
 
-        margin-left: .75rem;
-    }
+		margin-left: 0.75rem;
+	}
 
-    .navbar-wrapper aside a {
-        position: relative;
+	.navbar-wrapper aside a {
+		position: relative;
 
-        margin-left: 1rem;
-        padding-bottom: .5rem;
-    }
+		margin-left: 1rem;
+		padding-bottom: 0.5rem;
+	}
 
-    .navbar-wrapper aside a::before {
-        content: '';
+	.navbar-wrapper aside a::before {
+		content: '';
 
-        display: block;
-        width: 100%;
-        height: 3px;
+		display: block;
+		width: 100%;
+		height: 3px;
 
-        position: absolute;
-        bottom: 0;
+		position: absolute;
+		bottom: 0;
 
-        background-color: var(--green);
-        
-        border-radius: 8px;
+		background-color: var(--green);
 
-        transform: scaleX(0);
-        transform-origin: right;
+		border-radius: 8px;
 
-        transition: transform 300ms;
-    }
-    .navbar-wrapper aside a:hover::before {
-        transform: scaleX(1);
-        transform-origin: left;
-    }
+		transform: scaleX(0);
+		transform-origin: right;
+
+		transition: transform 300ms;
+	}
+	.navbar-wrapper aside a:hover::before {
+		transform: scaleX(1);
+		transform-origin: left;
+	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,15 +11,13 @@
 			<!-- <p>[29.10.2022] >>> { To pierwszy post! }</p> -->
 		</section>
 
-		<footer>
-			themixhelp, wszelkie prawa zastrzeżone
-		</footer>
+		<footer>themixhelp, wszelkie prawa zastrzeżone</footer>
 	</section>
 </template>
 
 <style scoped>
 	.home-wrapper {
-		min-height: calc(100vh - (2 * 2rem)); 
+		min-height: calc(100vh - (2 * 2rem));
 
 		display: grid;
 		grid-template-rows: auto 1fr auto;
